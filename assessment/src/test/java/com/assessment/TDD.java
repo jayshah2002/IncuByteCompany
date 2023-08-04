9f53b8cf7535a131320b302e2c05724983ee30c6
jayshah2002 <jaykshah2002@gmail.com>
2023-08-04 16:18:29 +0530

    BackwardCompleted

--- assessment/src/test/java/com/assessment/TDD.java
+++ assessment/src/test/java/com/assessment/TDD.java
@@ -20,23 +20,97 @@ public class TDD {
 	public void testMoveForward()
 	{
 	    // Test case 1
-	    spacecraft.moveForward();
-	    assertEquals("(0, 1, 0)", spacecraft.getPosition());
-	    assertEquals(Direction.N, spacecraft.getDirection());
+	    chandrayan.moveForward();
+	    assertEquals("(0, 1, 0)", chandrayan.getPosition());
+	    assertEquals(Direction.N, chandrayan.getDirection());
 
 	    // Test case 2
-	    spacecraft.setPosition(1, 0, 1);
-	    spacecraft.setDirection(Direction.E);
-	    spacecraft.moveForward();
-	    assertEquals("(2, 0, 1)", spacecraft.getPosition());
-	    assertEquals(Direction.E, spacecraft.getDirection());
+	    chandrayan.setPosition(1, 0, 1);
+	    chandrayan.setDirection(Direction.E);
+	    chandrayan.moveForward();
+	    assertEquals("(2, 0, 1)", chandrayan.getPosition());
+	    assertEquals(Direction.E, chandrayan.getDirection());
 
 	    // Test case 3
-	    spacecraft.setPosition(0, 0, 0);
-	    spacecraft.setDirection(Direction.W);
-	    spacecraft.moveForward();
-	    assertEquals("(-1, 0, 0)", spacecraft.getPosition());
-	    assertEquals(Direction.W, spacecraft.getDirection());
+	    chandrayan.setPosition(3,5,1);
+	    chandrayan.setDirection(Direction.W);
+	    chandrayan.moveForward();
+	    assertEquals("(-1, 0, 0)", chandrayan.getPosition());
+	    assertEquals(Direction.W, chandrayan.getDirection());
+
+		// Test case 4
+	    chandrayan.setPosition(0, 0, 0);
+	    chandrayan.setDirection(Direction.S);
+	    chandrayan.moveForward();
+	    assertEquals("(3,5,0)", chandrayan.getPosition());
+	    assertEquals(Direction.S, chandrayan.getDirection());
+
+		// Test case 5
+	    chandrayan.setPosition(-1, -1, -1);
+	    chandrayan.setDirection(Direction.S);
+	    chandrayan.moveForward();
+	    assertEquals("(-1, -1, -2)", chandrayan.getPosition());
+	    assertEquals(Direction.S, chandrayan.getDirection());
+
+		// Test case 6
+	    chandrayan.setPosition(2,-3,4);
+	    chandrayan.setDirection(Direction.W);
+	    chandrayan.moveForward();
+	    assertEquals("(-1, -1, -2)", chandrayan.getPosition());
+	    assertEquals(Direction.W, chandrayan.getDirection());
+	}
+
+	//Initially it will be failed
+	@Test
+	public void testMoveBackward()
+	{
+	    // Test case 1
+	    chandrayan.moveBackward();
+	    assertEquals("(0, 1, 0)", chandrayan.getPosition());
+	    assertEquals(Direction.N, chandrayan.getDirection());
+
+	    // Test case 2
+	    chandrayan.setPosition(0,0,0);
+	    chandrayan.setDirection(Direction.S);
+	    chandrayan.moveBackward();
+	    assertEquals("(0,-1,0)", chandrayan.getPosition());
+	    assertEquals(Direction.S, chandrayan.getDirection());
+
+	    // Test case 3
+	    chandrayan.setPosition(-1,-1,-1);
+	    chandrayan.setDirection(Direction.S);
+	    chandrayan.moveBackward();
+	    assertEquals("(-1,-1,-2)", chandrayan.getPosition());
+	    assertEquals(Direction.S, chandrayan.getDirection());
+
+		// Test case 4
+	    chandrayan.setPosition(3,5,1);
+	    chandrayan.setDirection(Direction.S);
+	    chandrayan.moveBackward();
+	    assertEquals("(3,5,2)", chandrayan.getPosition());
+	    assertEquals(Direction.S, chandrayan.getDirection());
+
+		// Test case 5
+	    chandrayan.setPosition(0,0,0);
+	    chandrayan.setDirection(Direction.W);
+	    chandrayan.moveBackward();
+	    assertEquals("(1,0,0)", chandrayan.getPosition());
+	    assertEquals(Direction.W, chandrayan.getDirection());
+
+		// Test case 6
+	    chandrayan.setPosition(1,0,-1);
+	    chandrayan.setDirection(Direction.E);
+	    chandrayan.moveBackward();
+	    assertEquals("(0, 0, 1)", chandrayan.getPosition());
+	    assertEquals(Direction.E, chandrayan.getDirection());
 	}
    
 }
+// {1, 0, 1, Direction.E, "b", "(0, 0, 1)", Direction.E},
+//                 {0, 0, 0, Direction.W, "b", "(1, 0, 0)", Direction.W},
+//                 {0, 0, 0, Direction.N, "b", "(0, -1, 0)", Direction.S},
+
+//                 {-1, -1, -1, Direction.S, "b", "(-1, -1, -2)", Direction.S},
+//                 {2, -3, 4, Direction.W, "b", "(2, -3, 3)", Direction.W},
+//                 {0, 0, 0, Direction.E, "b", "(-1, 0, 0)", Direction.E},
+//                 {3, 5, 1, Direction.S, "b", "(3, 5, 2)", Direction.S},
