8e8e75b1f7fd25bcfe7f17f04b3a6dfdc28f164b
jayshah2002 <jaykshah2002@gmail.com>
2023-08-04 15:31:37 +0530

    Intial Testcase Pass

---
+++ assessment/src/main/java/com/assessment/Chandrayan.java
@@ -0,0 +1,47 @@
+package com.assessment;
+
+public class Chandrayan {
+
+	 int x, y, z;
+	    char direction;
+
+	    public Chandrayan(int x, int y, int z, char direction) {
+	        this.x = x;
+	        this.y = y;
+	        this.z = z;
+	        this.direction = direction;
+	    }
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public int getZ() {
+			return z;
+		}
+
+		public void setZ(int z) {
+			this.z = z;
+		}
+
+		public char getDirection() {
+			return direction;
+		}
+
+		public void setDirection(char direction) {
+			this.direction = direction;
+		}
+	    
+}
---
+++ assessment/src/test/java/com/assessment/TDD.java
@@ -0,0 +1,14 @@
+package com.assessment;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TDD {
+	@Test
+	//sample test case 
+	public void Sample()
+	{
+		assertTrue(true);
+	}
+}
